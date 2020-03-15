54f417f39c800c0242401f7317e85747061daa1d
barisalbyrak <43125125+barisalbyrak@users.noreply.github.com>
2020-03-15 05:56:17 +0300

    Create arg0.js

---
+++ arg0.js
@@ -0,0 +1,5 @@
+/* 
+  Anti AdBlocks [7/5/18]
+  - https://www.detectadblock.com/
+*/
+var ykrd1=Math.random().toString(36).substring(2,15)+Math.random().toString(36).substring(2,15),ykrd2=Math.random().toString(36).substring(2,7),e=document.getElementById(&quot;ykth&quot;);e.setAttribute(&quot;id&quot;,ykrd1),e.classList.add(&quot;ykwrp_&quot;+ykrd2),window.showAds||(document.getElementById(ykrd1).style.display=&quot;block&quot;);
